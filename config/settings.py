f96cb5cfc770626cdb0c0594df772b87be2e9829
fazletdinov <fvi-it@mail.ru>
2022-08-20 17:50:59 +0500

    Изменили настройки

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
- 'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- }
+ #'default': {
+ #   'ENGINE': 'django.db.backends.sqlite3',
+ #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #}
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
