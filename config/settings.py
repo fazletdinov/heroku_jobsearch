0082339986a729082ea07d76dd404702cb2000bf
fazletdinov <fvi-it@mail.ru>
2022-08-20 18:00:51 +0500

    Изменили настройки

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
- #'default': {
- #   'ENGINE': 'django.db.backends.sqlite3',
- #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- #}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "job",
+        "USER": "jobuser",
+        "PASSWORD": "gjbcrhfbjns",
+        #"HOST": "localhost",
+        #"PORT": "5432",
+    }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
