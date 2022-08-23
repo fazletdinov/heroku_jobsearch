1b7875e7aceae13a9b19dcb0ea92326c44cd3c9a
fazletdinov <fvi-it@mail.ru>
2022-08-23 11:50:00 +0500

    Изменили настройки

--- config/settings.py
+++ config/settings.py
@@ -166,7 +166,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
