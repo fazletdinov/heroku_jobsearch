ee9772141d25b5ca799b2dadc5a6e1d11c7c5a8e
fazletdinov <fvi-it@mail.ru>
2022-08-29 21:41:07 +0500

    Изменили настройки

--- users/utils.py
+++ users/utils.py
@@ -3,6 +3,6 @@ from django.core.mail import EmailMessage
 class Util:
     @classmethod
     def send_email(data):
-        email = EmailMessage(subject=data['email_subject'],
-                             body=data['email_body'])
+        email = EmailMessage(
+            subject=data['email_subject'], body=data['email_body'], to=data['to_email'])
         email.send()
