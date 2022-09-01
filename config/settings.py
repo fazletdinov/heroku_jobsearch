d828bda0847bae695ceaf8a3c14144a58b535c81
fazletdinov <fvi-it@mail.ru>
2022-09-01 13:39:04 +0500

    Изменили настройки

--- config/settings.py
+++ config/settings.py
@@ -254,11 +254,11 @@ django_heroku.settings(locals())
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bbomjur@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bomjur12345'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'Freedomitcoty@yandex.ru'
+EMAIL_HOST_PASSWORD = 'ennntybladzveiir'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 
 #DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 #SERVER_EMAIL = EMAIL_HOST_USER
