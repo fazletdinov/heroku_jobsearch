8098d798a83d67837f37fd1e00a83ec9ad4f2841
fazletdinov <fvi-it@mail.ru>
2022-09-01 14:53:06 +0500

    Изменили настройки

--- users/utils.py
+++ users/utils.py
@@ -1,8 +1,10 @@
 from django.core.mail import EmailMessage
 
+
 class Util:
     @staticmethod
     def send_email(data):
         email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
+            subject=data['email_subject'], body=data['email_body'],
+            from_email='Freedomitcoty@yandex.ru', to=[data['to_email']])
         email.send()
