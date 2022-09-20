f7cea608cdefcc2dc9b608775c9a6debc0f7bfe5
fazletdinov <fvi-it@mail.ru>
2022-09-20 17:15:30 +0500

    очистили базу и сделали миграции

--- users/signals.py
+++ users/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import MyUser, UserProfile
 
-@receiver(post_save, sender=MyUser)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+#@receiver(post_save, sender=MyUser)
+#def create_profile(sender, instance, created, **kwargs):
+#    if created:
+#        UserProfile.objects.create(user=instance)
